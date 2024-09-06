844e995bb521a1d53208833f77bd5651f48fecb0
Kim-Su-B <hbsubin1@naver.com>
2024-09-06 17:16:21 +0900

    Join.jsx, Login.jsx 디자인 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main'; 
 import Login from './pages/Login/Login';
+import Join from './pages/Join/Join';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<Main/>}/>
 
         <Route path='/Login' element={<Login/>}/>
+        <Route path='/Join' element={<Join/>}/>
       </Routes>
     </Router>
   );
