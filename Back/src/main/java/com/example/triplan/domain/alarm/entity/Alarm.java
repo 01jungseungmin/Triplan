aaff9ca8c01f8089964744ec716007ad1cc5d933
lby9906 <lby9906@naver.com>
2024-10-11 16:15:40 +0900

    feat: 그룹 초대 및 알림 조회, 그룹 초대 수락/거절 구현

--- Back/src/main/java/com/example/triplan/domain/alarm/entity/Alarm.java
+++ Back/src/main/java/com/example/triplan/domain/alarm/entity/Alarm.java
@@ -1,21 +1,35 @@
 package com.example.triplan.domain.alarm.entity;
 
 import com.example.triplan.common.BaseEntity;
+import com.example.triplan.domain.account.entity.Account;
 import com.example.triplan.domain.alarm.enums.AlarmType;
 import com.example.triplan.domain.crew.entity.Crew;
+import com.example.triplan.domain.crew.entity.CrewList;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
+@RequiredArgsConstructor
 public class Alarm extends BaseEntity {
-    @ManyToOne
-    @JoinColumn(name = "crew_id")
-    private Crew crew;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_list")
+    private CrewList crewList;
 
     @Enumerated(EnumType.STRING)
-    @Schema(description = "그룹 초대 수락 상태값", example = "ACCEPT, DECLINE, WAIT")
+    @Schema(description = "알림 타입", example = "INVITE")
     @Column(name ="alarmType", nullable = false)
     private AlarmType alarmType;
+
+    public Alarm(AlarmType alarmType, Account account, CrewList crewList) {
+        this.alarmType = alarmType;
+        this.account = account;
+        this.crewList = crewList;
+    }
 }
--- Back/src/main/java/com/example/triplan/domain/crew/repository/CrewRepository.java
+++ Back/src/main/java/com/example/triplan/domain/crew/repository/CrewRepository.java
@@ -5,6 +5,10 @@ import com.example.triplan.domain.crew.entity.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CrewRepository extends JpaRepository<Crew, Long>{
+
+    List<Crew> findAllByAccount(Account account);
 }
