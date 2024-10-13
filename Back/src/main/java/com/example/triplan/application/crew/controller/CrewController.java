64abd131d1f3a738079d602d8157e1e6e3b8d52b
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2024-10-13 19:21:04 +0900

    그룹 출력

--- Back/src/main/java/com/example/triplan/application/crew/controller/CrewController.java
+++ Back/src/main/java/com/example/triplan/application/crew/controller/CrewController.java
@@ -2,23 +2,39 @@ package com.example.triplan.application.crew.controller;
 
 import com.example.triplan.application.crew.dto.request.CrewRequest;
 import com.example.triplan.application.crew.dto.response.CrewResponse;
+import com.example.triplan.application.crew.service.CrewReadService;
 import com.example.triplan.application.crew.service.CrewWriteService;
-import com.example.triplan.domain.crew.entity.Crew;
-import com.example.triplan.exception.CustomAuthenticationException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
 public class CrewController {
     private final CrewWriteService crewWriteService;
+    private final CrewReadService crewReadService;
 
     @PostMapping("/crew/create")
     public ResponseEntity<CrewResponse> createCrew(@RequestBody CrewRequest crewRequest){
         return ResponseEntity.ok(crewWriteService.create(crewRequest));
     }
+
+    // 전체 그룹 리스트 조회
+    @GetMapping("/crew/list")
+    public ResponseEntity<List<CrewResponse>> crewList() {
+        List<CrewResponse> crewList = crewReadService.findAllCrew();
+        return ResponseEntity.ok(crewList);
+    }
+
+    // 특정 그룹 정보 조회
+    @GetMapping("/crew/list/{crewId}")
+    public ResponseEntity<CrewResponse> getCrew(@PathVariable Long crewId) {
+        Optional<CrewResponse> crew = crewReadService.findCrew(crewId);
+        return crew.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
