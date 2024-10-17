cdaa0bf24046fdb0d0db3758e461d0b88208f926
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2024-10-17 19:47:14 +0900

    그룹별 여행 일정 출력

--- Back/src/main/java/com/example/triplan/application/plan/dto/response/PlanResponse.java
+++ Back/src/main/java/com/example/triplan/application/plan/dto/response/PlanResponse.java
@@ -3,16 +3,26 @@ package com.example.triplan.application.plan.dto.response;
 import com.example.triplan.domain.crew.entity.Crew;
 import com.example.triplan.domain.place.entity.Place;
 import com.example.triplan.domain.plan.entity.Plan;
+import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Getter
 public class PlanResponse {
     private LocalDate planDate;
     private LocalTime planStartTime;
     private String planMemo;
     private Long placeId;
     private Long crewId;
+
+    public PlanResponse(LocalDate planDate, LocalTime planStartTime, String planMemo, Long placeId, Long crewId) {
+        this.planDate = planDate;
+        this.planStartTime = planStartTime;
+        this.planMemo = planMemo;
+        this.placeId = placeId;
+        this.crewId = crewId;
+    }
 }
 
 
