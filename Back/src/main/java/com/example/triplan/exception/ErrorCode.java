1f89715096b18e9a79a25c5301a3dfd8aca55c5c
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2024-11-30 14:08:52 +0900

    게시판 이미지 업로드

--- Back/src/main/java/com/example/triplan/exception/ErrorCode.java
+++ Back/src/main/java/com/example/triplan/exception/ErrorCode.java
@@ -13,7 +13,12 @@ public enum ErrorCode {
     INVITATION_ALREADY_SENT(HttpStatus.BAD_REQUEST, "이미 초대 된 회원입니다."),
     ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "알림을 찾을 수 없습니다."),
     PLAN_NOT_FOUNT(HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다."),
-    PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "장소를 찾을 수 없습니다.");
+    PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "장소를 찾을 수 없습니다."),
+    EMPTY_FILE_EXCEPTION(HttpStatus.NOT_FOUND,"파일이 올라가지 않습니다"),
+    IO_EXCEPTION_ON_IMAGE_UPLOAD(HttpStatus.NOT_FOUND,"파일이 올라가지 않습니다"),
+    NO_FILE_EXTENTION(HttpStatus.NOT_FOUND,"파일이 존재하지 않습니다"),
+    INVALID_FILE_EXTENTION(HttpStatus.NOT_FOUND,"파일이 존재하지 않습니다"),
+    PUT_OBJECT_EXCEPTION(HttpStatus.NOT_FOUND,"파일 전송 오류 발생");
 
     private final HttpStatus httpStatus;
     private final String message;
