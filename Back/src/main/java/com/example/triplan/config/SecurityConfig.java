63697232ebdff8cfe52448ff52f7fa8d71791b5e
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2024-12-10 10:04:11 +0900

    관리자 장소추가 버튼 위치 조정

--- Back/src/main/java/com/example/triplan/config/SecurityConfig.java
+++ Back/src/main/java/com/example/triplan/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // 세션을 사용하지 않기 때문에 STATELESS로 설정
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/", "/static/**","index.html","/swagger-ui/**","/{boardId}/answer" ,"/v3/api-docs/**","/login", "/join", "/main", "/api/load","/api/boards","/place/findAll", "/place/details/{placeId}", "/api/boards/{boardId}").permitAll() // 로그인, 회원가입, 메인 페이지는 모두 접근 가능
+                        .requestMatchers("/", "/static/**","index.html","/favicon.ico","/swagger-ui/**","/{boardId}/answer" ,"/v3/api-docs/**","/login", "/join", "/main", "/api/load","/api/boards","/place/findAll", "/place/details/{placeId}", "/api/boards/{boardId}").permitAll() // 로그인, 회원가입, 메인 페이지는 모두 접근 가능
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
                 .with(new JwtSecurityConfig(tokenProvider), customizer -> {}); //filterChain 등록
