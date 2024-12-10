8bba0e0a1032a0fb68953eddc3b9dbdd71bcd3f0
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2024-12-10 15:01:48 +0900

    서버 주소로 변경

--- Back/src/main/java/com/example/triplan/config/SecurityConfig.java
+++ Back/src/main/java/com/example/triplan/config/SecurityConfig.java
@@ -47,13 +47,8 @@ public class SecurityConfig {
 
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // 세션을 사용하지 않기 때문에 STATELESS로 설정
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/", "/favicon.ico","/swagger-ui/**","/{boardId}/answer" ,"/v3/api-docs/**","/login", "/join", "/main", "/api/load","/api/boards","/place/findAll", "/place/details/{placeId}", "/api/boards/{boardId}").permitAll() // 로그인, 회원가입, 메인 페이지는 모두 접근 가능
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-
-                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
-                )
-                .with(new JwtSecurityConfig(tokenProvider), customizer -> {}); //filterChain 등록
-
+                        .anyRequest().permitAll() // 모든 요청을 허용
+                );
 
 
         return http.build();
