83d99981346a72e4496cc9dca97156184c6c06aa
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2024-12-16 20:28:38 +0900

    서버 주소로 변경

--- Front/src/components/BestBoard.jsx
+++ Front/src/components/BestBoard.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../components/css/BestBoard.css';
 import PlaceBoardItem from './PlaceBoardItem';
-import { Link } from 'react-router-dom';
 
 function BestBoard() {
   const [topPlaces, setTopPlaces] = useState([]);
@@ -10,35 +9,30 @@ function BestBoard() {
 
   useEffect(() => {
     const fetchTopPlaces = async () => {
-        try {
-            // 로그인 상태에 관계없이 인증이 필요 없는 요청
-            const response = await fetch('http://13.209.211.218:8080/place/findAll', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json', // 인증 헤더를 보내지 않음
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error(`장소 데이터를 불러오지 못했습니다. 상태 코드: ${response.status}`);
-            }
-            const data = await response.json();
-            console.log('API에서 받아온 데이터:', data);
-
-            // count를 기준으로 정렬하고 상위 3개를 선택
-            const sortedPlaces = data
-                .sort((a, b) => Number(b.count) - Number(a.count)) // 숫자로 변환하여 정렬
-                .slice(0, 3);
-            setTopPlaces(sortedPlaces);
-        } catch (error) {
-            console.error('에러 발생:', error);
-        } finally {
-            setLoading(false); // 로딩 상태 해제
+      try {
+        const response = await fetch('http://13.209.211.218:8080/place/findAll', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+  
+        if (!response.ok) {
+          throw new Error(`장소 데이터를 불러오지 못했습니다. 상태 코드: ${response.status}`);
         }
+        const data = await response.json();
+        console.log('API에서 받아온 데이터:', data);
+        setPlaces(data); // 받아온 데이터를 상태에 저장
+        setLoading(false); // 로딩 상태 해제
+      } catch (error) {
+        console.error('에러 발생:', error);
+        setError('장소 데이터를 불러오는 중 오류가 발생했습니다.');
+        setLoading(false); // 로딩 상태 해제
+      }
     };
-
+  
     fetchTopPlaces();
-}, []);
+  }, []);
 
   // 로딩 상태가 끝난 후 렌더링
   if (loading) return null; // 로딩 중 상태 제거
@@ -48,8 +42,8 @@ function BestBoard() {
     <div className="best-Board-container">
       <div className="BestBoard-header-box">
         <div className="BestBoard-header">BEST PLACE</div>
-        <Link to="/PlaceBoard">전체보기</Link>
-        </div>
+        <a href="/PlaceBoard">전체보기</a>
+     </div>
       <div className="BestBoardSub">사람들이 많이 찾는 장소에요</div>
       <div className="best-board-grid">
         {topPlaces.map((place, index) => (
