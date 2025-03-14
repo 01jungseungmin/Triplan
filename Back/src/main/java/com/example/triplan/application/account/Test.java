31a36892a79dc19c5caa9e21702fac3a7d56e625
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2025-03-14 16:28:58 +0900

    ci/cd 테스트2

---
+++ Back/src/main/java/com/example/triplan/application/account/Test.java
@@ -0,0 +1,8 @@
+package com.example.triplan.application.account;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+}
