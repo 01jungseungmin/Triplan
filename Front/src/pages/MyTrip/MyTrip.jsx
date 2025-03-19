98d82e746bde72f7aa889fad6cea5062ce2530a5
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2025-03-19 17:29:08 +0900

    img 변경

--- Front/src/pages/MyTrip/MyTrip.jsx
+++ Front/src/pages/MyTrip/MyTrip.jsx
@@ -193,8 +193,9 @@ function MyTrip() {
                     <div className="modal">
                         <div className="form-container">
                             <div className="image-section">
+                                
                                 <img
-                                    src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA1MTNfMTAz%2FMDAxNjIwODgyMTQ1NzYx.PFIlobbvbexGiulZmt59rbm5NkH9KcEnZR3A-lGAk2Yg.W9N1TZka_suiFjasFi11tKiumUtlztw5vqn-YLpPEMAg.JPEG.xoxo_0717%2FIMG_3538.JPG&type=a340"
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoQoOS9Fb2tIZQI8knvJbitYcvaXZaKY-3iYpR6GZA9qwic0cS9LfJz4Y&s"
                                     alt="background"
                                     className="background-image"
                                 />
