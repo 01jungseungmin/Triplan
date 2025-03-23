8527a205103b864e95eb504fca3d2be3107dde0c
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2025-03-23 19:25:16 +0900

    jenkins 테스트 게시글 수정

--- Back/src/main/java/com/example/triplan/application/board/service/BoardWriteService.java
+++ Back/src/main/java/com/example/triplan/application/board/service/BoardWriteService.java
@@ -71,14 +71,14 @@ public class BoardWriteService {
 
     // 게시글 수정
     public String update(Long boardId, UpdateBoardRequest updateBoardRequest) {
-        // Account account = accountService.getCurrentUser(); // 현재 로그인된 사용자 정보 가져오기
+        Account account = accountService.getCurrentUser(); // 현재 로그인된 사용자 정보 가져오기
 
         Board board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
 
-        /* if (!board.getAccount().getId().equals(account.getId())) {
+        if (!board.getAccount().getId().equals(account.getId())) {
             throw new RuntimeException("권한이 없습니다.");
-        }*/
+        }
         board.update(updateBoardRequest.getTitle(), updateBoardRequest.getContent());
         return "게시글 수정 완료";
     }
